acbb5f87f4110a55050bb76914663192db430d4c
nick-kuh <142516148+nick-kuh@users.noreply.github.com>
2024-09-12 21:09:09 +0000

    exceção

---
+++ Excecoes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class MinhaExcecoes{
+    public:
+        string mensagem;
+
+        MinhaExcecoes(string s){
+            mensagem = s;
+        }
+};
