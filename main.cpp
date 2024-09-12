da88365e53f9f763529cde1fb2a9ec39ff0db02f
nick-kuh <142516148+nick-kuh@users.noreply.github.com>
2024-09-12 00:49:56 +0000

    Classe Pessoa

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "Pessoa.cpp"
+using namespace std;
 
 int main(){
-    std::cout << "hello world\n";
-}
+    Pessoa p;
+    p.setNome("Nick");
+    p.setSobrenome("Kuhnast");
+    p.setCpf(123456);
+
+    cout << "Dados da pessoa: " << endl;
+    cout << "Nome: " << p.getNome() << endl;
+    cout << "Sobrenome: " << p.getSobrenome() << endl;
+    cout << "CPF: " << p.getCpf() << endl;
+};
